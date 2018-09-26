dfcd6d91eb133ab5cec9c1ab6124779c625f12f7
Ava Barron <mztriz@gmail.com>
2018-09-26 21:12:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,11 +7,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(boad, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      return true
-    end
-  else
-    return false
-  end
+  if index.between?(0,8) && !position_taken?(board, index)
 end
